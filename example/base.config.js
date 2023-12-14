54733adb5fa543763dc5625a2e28e2a3c38da766
veeui <m15037392885@126.com>
2023-12-15 02:26:44 +0800

    chore: 5.0.1发包

--- example/base.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    title: `管理系统${process.env.VUE_APP_MOCK ? 'Mock' : ''}`,
-    baseName: 'api',
-    mockName: 'mock',
-    proxyUrl: 'http://localhost:4000',
-    mockUrl: 'http://localhost:9009'
-}
--- example/src/api/auth.js
+++
@@ -1,8 +0,0 @@
-const { baseName, mockName } = require('../../base.config');
-import BaseAxios from '@/utils/http';
-
-const APILogin = async params => await BaseAxios.post(`${process.env.VUE_APP_MOCK ? mockName : baseName}/auth/login`, params);
-
-export {
-    APILogin,
-}
